9c7aaadf68a5d687d51c2d83545a60d5f578c3c5
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-06-07 16:57:27 +0300

    Update simple_happiness_calculator.py

--- simple_happiness_calculator.py
+++ simple_happiness_calculator.py
@@ -1,4 +1,6 @@
 import streamlit as st
+
+# Function to calculate happiness using the provided formula
 def calculate_happiness(alpha, beta, income, age, education, sleep_duration, error_terms):
     # Normalize the input variables
     normalized_income = income / 1000000
@@ -19,19 +21,9 @@ def calculate_happiness(alpha, beta, income, age, education, sleep_duration, err
     normalized_happiness = (happiness - min_happiness) / (max_happiness - min_happiness)
 
     return normalized_happiness
-# Function to normalize the error variables
-def normalize_error(error):
-    # Define the minimum and maximum values for each error variable
-    if error == "error1":
-        min_value = -10
-        max_value = 10
-    elif error == "error2":
-        min_value = -15
-        max_value = 0
-    elif error == "error3":
-        min_value = 0
-        max_value = 1
 
+# Function to normalize the error variables
+def normalize_error(error, min_value, max_value):
     # Normalize the error value
     normalized_error = (error - min_value) / (max_value - min_value)
 
@@ -47,10 +39,10 @@ def main():
     st.title("Input Variables")
     alpha = st.slider("How happy are your parent(s)/guardian", 0, 100, 50)
     beta = st.slider("How well do you believe in love", 0.0, 1.0, 0.5)
-    income = st.slider("Income", 0, 1000000, 50)
+    income = st.slider("Income", 0, 1000000, 50000)
     age = st.slider("Age", 7, 100, 7)
     education = st.slider("How Educated are you", 0, 100, 10)
-    sleep_duration = st.slider("What your last Sleep Duration (hours)", 0, 12, 8)
+    sleep_duration = st.slider("What your last Sleep Duration (hours)", 0, 24, 8)
 
     # Error terms
     st.title("Personality and Life choices")
@@ -59,8 +51,8 @@ def main():
     error3 = st.checkbox("I believe in: Charity, Resilience, Moderation or Hard work")
 
     # Normalize the error variables
-    normalized_error1 = normalize_error(error1)
-    normalized_error2 = normalize_error(0 if error2 == "No" else -10 if error2 == "Medium" else -15)
+    normalized_error1 = normalize_error(error1, -10, 10)
+    normalized_error2 = normalize_error(0 if error2 == "No" else -10 if error2 == "Medium" else -15, -15, 0)
     normalized_error3 = 1 if error3 else 0
 
     error_terms = [normalized_error1, normalized_error2, normalized_error3]
@@ -69,13 +61,13 @@ def main():
     happiness = calculate_happiness(alpha, beta, income, age, education, sleep_duration, error_terms)
 
     # Normalize happiness between 0 and 1
-    #normalized_happiness = normalize_happiness(happiness)
+    normalized_happiness = happiness if happiness <= 1.0 else 1.0
 
     # Display happiness
     st.sidebar.write("")
-    if st.sidebar.button('Calculate my Happinesss'):
+    if st.sidebar.button('Calculate my Happiness'):
         st.sidebar.subheader("Happiness Score:")
-        st.sidebar.write(happiness)
+        st.sidebar.write(normalized_happiness)
 
 # Run the Streamlit application
 if __name__ == '__main__':
