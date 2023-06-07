f1c9b151312b44efd2e2392450d7d0ce34f6b38b
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-06-07 07:14:13 +0300

    Update simple_happiness_calculator.py

--- simple_happiness_calculator.py
+++ simple_happiness_calculator.py
@@ -32,7 +32,7 @@ def main():
     st.title("Input Variables")
     alpha = st.slider("How happy are your parent(s)/guardian", 0, 100, 50)
     beta = st.slider("How well do you believe in love", 0.0, 1.0, 0.5)
-    income = st.slider("Income", 0, 1000000000, 50)
+    income = st.slider("Income", 0, 1000000, 50)
     age = st.slider("Age", 7, 100, 7)
     education = st.slider("How Educated are you", 0, 100, 10)
     sleep_duration = st.slider("What your last Sleep Duration (hours)", 0, 12, 8)
