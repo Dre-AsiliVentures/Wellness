cee3e0a6fac4095e94e8b81d0e867bfc8a24622e
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-06-13 18:52:09 +0300

    Update friendship_calculator.py

--- friendship_calculator.py
+++ friendship_calculator.py
@@ -3,24 +3,24 @@ import streamlit as st
 def calculate_friendship(proximity, duration, frequency, intensity):
     score = proximity + duration + frequency + intensity
     
-    if score <= 4:
+    if proximity <= 2:
         return "Strangers"
     elif score <= 8:
         return "Acquaintance"
-    elif score <= 12:
+    elif intensity >3 and score <= 12:
         return "Close Friends"
-    else:
+    elif intesity>=4 and duration>3 and proximity>=3:
         return "Significant Other"
+    else: 
+        return ""
 
 def main():
     st.title("Friendship Calculator")
 
-    st.sidebar.title("Friendship Calculator")
-
-    proximity = st.sidebar.slider("Proximity", 0, 10, 5)
-    duration = st.sidebar.slider("Duration", 0, 10, 5)
-    frequency = st.sidebar.slider("Frequency", 0, 10, 5)
-    intensity = st.sidebar.slider("Intensity", 0, 10, 5)
+    proximity = st.slider("Proximity", 0, 10, 5)
+    duration = st.slider("Duration", 0, 10, 5)
+    frequency = st.slider("Frequency", 0, 10, 5)
+    intensity = st.slider("Intensity", 0, 10, 5)
 
     result = calculate_friendship(proximity, duration, frequency, intensity)
 
