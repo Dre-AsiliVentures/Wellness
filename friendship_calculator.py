577ef5e84a1fbfd40b5bae5f59ecc4cbadd73b78
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-06-13 21:08:54 +0300

    Update friendship_calculator.py

--- friendship_calculator.py
+++ friendship_calculator.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 def calculate_friendship(proximity, duration, frequency, intensity):
     score = proximity + duration + frequency + intensity
-    if proximity <= 2 and duration <= 2 and frequency <= 2 and intensity <= 2:
+    if (proximity <= 2 or duration <= 2) and frequency <= 2 and intensity <= 2:
         return "Strangers"
-    elif proximity <= 4 and duration <= 4 and frequency <= 4 and intensity <= 4:
+    elif proximity <= 4 and (duration <= 4 or frequency <= 4) and intensity <= 4:
         return "Acquaintance"
     elif proximity <= 6 and duration <= 6 and frequency <= 6 and intensity <= 6:
         return "Close Friends"
@@ -12,6 +12,21 @@ def calculate_friendship(proximity, duration, frequency, intensity):
         return "Significant Other"
     else:
         return "Friendship level not defined"
+
+#     if proximity <= 2 or duration <= 2:
+#         return "Strangers"
+#     elif frequency <= 2 and intensity <= 2:
+#         return "Strangers"
+#     elif proximity <= 4 or duration <= 4:
+#         return "Acquaintance"
+#     elif frequency <= 4 and intensity <= 4:
+#         return "Acquaintance"
+#     elif proximity <= 6 and duration <= 6 and frequency <= 6 and intensity <= 6:
+#         return "Close Friends"
+#     elif proximity >= 8 and duration >= 8 and frequency >= 8 and intensity >= 8:
+#         return "Significant Other"
+#     else:
+#         return "Friendship level not defined"
 
 def main():
     st.title("Friendship Calculator")
