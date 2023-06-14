f30d1470431e776f3f03ce1a0766cc03f14dd020
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-06-14 08:12:23 +0300

    Update friendship_calculator.py

--- friendship_calculator.py
+++ friendship_calculator.py
@@ -43,10 +43,10 @@ def main():
         if st.sidebar.button('Reset'):
             #st.caching.clear_cache()  # Clear the cache
             #Reset the slider values to their default values
-            st.session_state["proximity"] = 5
-            st.session_state["duration"] = 5
-            st.session_state["frequency"] = 5
-            st.session_state["intensity"] = 5
+            proximity = st.slider("Proximity", 0, 10, 5)
+            duration = st.slider("Duration", 0, 10, 5)
+            frequency = st.slider("Frequency", 0, 10, 5)
+            intensity = st.slider("Intensity", 0, 10, 5)
 #             proximity.value = 5
 #             duration.value = 5
 #             frequency.value = 5
