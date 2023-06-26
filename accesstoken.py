1ca2aa0e058c7cd0368b82f4c830c1c846384c01
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-06-26 21:54:50 +0300

    Update accesstoken.py

--- accesstoken.py
+++ accesstoken.py
@@ -2,7 +2,8 @@ import streamlit as st
 import os
 
 # Retrieve the value of the secret
-my_secret = os.environ.get('wellnessguru')
+#my_secret = os.environ.get('wellnessguru')
+my_secret = os.environ["wellnessguru"]
 
 # Use the secret in your Streamlit application
 st.write("WELLNESS Secret Password:", my_secret)
