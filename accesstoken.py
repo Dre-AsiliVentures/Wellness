db4bef84891ad6dcbcf9382e862205618b74630b
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-06-27 06:18:22 +0300

    Update accesstoken.py
    Line 14 Using decouple module

--- accesstoken.py
+++ accesstoken.py
@@ -1,8 +1,14 @@
-import streamlit as st
+from decouple import config
 import os
+import streamlit as st
+
+
 
 # Retrieve the value of the secret
+new_secret = config('MY_SECRET_NAME')
+
 my_secret = os.environ.get('WELLNESSGURU')
 
 # Use the secret in your Streamlit application
 st.write("WELLNESS Secret Password:", my_secret)
+st.write("WELLNESS New Secret Password:", new_secret)
