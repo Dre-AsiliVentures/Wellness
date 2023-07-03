e12c456fe211142f74771e887edf90ac52b5896c
Dre-AsiliVentures <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-07-03 05:56:32 +0300

    Update wellness api
    Wellness api protected with gitignore

--- accesstoken.py
+++ accesstoken.py
@@ -1,15 +1,21 @@
+from dotenv import load_dotenv
 import os
 import streamlit as st
 
+
+load_dotenv()  # Load the secrets from .env file
+
+# Access secrets using os.getenv('SECRET_NAME')
+wellness_api_key = os.getenv('WELLNESSAPI_KEY')
 # headers={
 #   "authorization":st.secrets["WELLNESSGURU"]
 # }
 st.write(os.environ.items())
 
 #my_secret = os.environ.get('WELLNESSGURU')
-my_secret = os.environ['WELLNESSGURU']
+#my_secret = os.environ['WELLNESSGURU']
 
 # Use the secret in your Streamlit application
-st.write("WELLNESS Secret Password:", my_secret)
+st.write("WELLNESS Secret Password:", wellness_api_key)
 #st.write("WELLNESS New Secret Password:", new_secret)
 
